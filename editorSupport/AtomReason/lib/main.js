9299884d27aec89d250a79e57cb90a66e881a1c7
Cheng Lou <chenglou92@gmail.com>
2016-04-13 21:13:09 -0700

    [AtomReason] Back to type hint!
    Type hint, as of Nuclide 0.130.0, now supports syntax highlighting and line breaks. We don't need to roll our own stuff anymore!
    This removes our dependency on React and highlight-native.
    
    @jordwalke

--- editorSupport/AtomReason/lib/main.js
+++ editorSupport/AtomReason/lib/main.js
@@ -89,16 +89,12 @@ module.exports = {
     };
   },
 
-  // Note that we're actually creating a nuclide-datatip provider, not
-  // nuclide-type-hint provider. We historically used type-hint provider, but it
-  // doesn't display rich content as of this diff. We'll switch back when I get
-  // bored enough to submit a PR against nuclide.
   createTypeHintProvider() {
     return {
-      validForScope: scope => scope === 'source.reason',
+      selector: 'source.reason',
       providerName: PACKAGE_NAME,
       inclusionPriority: 1,
-      datatip: AtomReason.getNuclideJsTypeHint,
+      typeHint: AtomReason.getNuclideJsTypeHint,
     };
   },
 
