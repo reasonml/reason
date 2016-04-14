378fca303a73d63c501ac07ed1751b9eaa3b93bd
David <xudavidb@gmail.com>
2016-04-14 00:47:07 -0700

    add tests for token reformatting

---
+++ formatTest/typeCheckedTests/expected_output/comments.re
@@ -0,0 +1,15 @@
+
+/** */
+/* **** comment */
+/*** comment */
+/** docstring */
+/* comment */
+/** docstring */
+/*** comment */
+/**** comment */
+/***** comment */
+/* *(*** comment *) */
+/* comment **/
+/* comment ***/
+/* comment ****/
+/* comment *****/
---
+++ formatTest/typeCheckedTests/input/comments.ml
@@ -0,0 +1,16 @@
+(** *)
+(* **** comment *)
+(*** comment *)
+(** docstring *)
+(* comment *)
+(** docstring *)
+(*** comment *)
+(**** comment *)
+(***** comment *)
+
+(* *(*** comment *) *)
+
+(* comment **)
+(* comment ***)
+(* comment ****)
+(* comment *****)
