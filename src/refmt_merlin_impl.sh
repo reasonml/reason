reasonml/reason

commit 92985c45996884a8e521ab977a0056966289d0a8

reasonfmt -> refmt (#360)

.gitignore
@@ -1,5 +1,6 @@
 *.native
 reasonfmt_merlin_impl.sh
+refmt_merlin_impl.sh
 *.byte
 *.docdir
 _build

Makefile
@@ -5,8 +5,8 @@ default: build test
 build:
 	cp pkg/META.in pkg/META
 	ocaml pkg/build.ml native=true native-dynlink=true utop=true
-	chmod +x $(shell pwd)/_build/src/reasonfmt_merlin_impl.sh
-	ln -fs $(shell pwd)/_build/src/reasonfmt_merlin_impl.sh reasonfmt_merlin_impl.sh
+	chmod +x $(shell pwd)/_build/src/refmt_merlin_impl.sh
+	ln -fs $(shell pwd)/_build/src/refmt_merlin_impl.sh refmt_merlin_impl.sh
 
 install:
 	opam pin add reason . -y

editorSupport/AtomReason/Reasonify/formatErrorMessages.js
@@ -7,7 +7,7 @@ var desiredTableMargin = 4;
 
 var config = {
   // Amount to expand source preview in each direction. If -1, shows no preview
-  pathToReasonfmt: 'reasonfmt',
+  pathToRefmt: 'refmt',
   showSourcePreviews: true,
   // Whether or not to show the file path/line in the error formatting
   showFileHeaders: true,
@@ -510,7 +510,7 @@ var KindPrinters = {
       var expectedEquivalentType = expectedEquivalentTypes[i];
       var niceInferredEquivalentType = inferredEquivalentType && Reasonify.niceifyType(inferredEquivalentType, config);
       var niceExpectedEquivalentType = expectedEquivalentType && Reasonify.niceifyType(expectedEquivalentType, config);
-      var addedInferredLines = 
+      var addedInferredLines =
         niceInferredEquivalentType ?
           reasonifyTypeLines(niceInferredEquivalentType, Math.round(config.columns / 2) - 4) : [];
       var addedExpectedLines =
@@ -555,7 +555,7 @@ var KindPrinters = {
           '  =',
           config
         ) : [];
-      var addedExpectedLines = 
+      var addedExpectedLines =
         expectedConflictEquivalentTypes ?
           joinArrays(
             Reasonify.reasonifyEquivalentTypeLines(

editorSupport/AtomReason/Reasonify/index.js
@@ -33,7 +33,7 @@ exports.reasonifyManySignatureItems = function(items, width, config) {
   // accurate as the Reason -> Reason printer.
   var mlText = items.join(' (*SPLIT*) ');
   var resplit = child_process.execSync(
-    config.pathToReasonfmt + ' -is-interface-pp true -use-stdin true -parse ml -print re -print-width ' + (width - 1),
+    config.pathToRefmt + ' -is-interface-pp true -use-stdin true -parse ml -print re -print-width ' + (width - 1),
     {input: mlText}
   ).toString().split('/*SPLIT*/');
   return resplit;
@@ -45,7 +45,7 @@ exports.reasonifyTypeLines = function(mlType, width, config) {
   return exports.removeBlankLines(
     exports.removeLeftPadding(
       exports.replaceDummyType(
-        child_process.execSync(config.pathToReasonfmt + ' -use-stdin true -parse ml -print re -print-width ' + (width - 1),
+        child_process.execSync(config.pathToRefmt + ' -use-stdin true -parse ml -print re -print-width ' + (width - 1),
           {input: mlType}
         ).toString()
       ).split('\n')
@@ -86,4 +86,3 @@ exports.reasonifyEquivalentTypeLines = function(mlTypes, width, config) {
 exports.reasonifyType = function(mlType, width, config) {
   return exports.reasonifyTypeLines(mlType, width, config).join('\n').trimRight();
 };
-

editorSupport/AtomReason/lib/main.js
@@ -121,12 +121,12 @@ module.exports = {
     }
   },
   config: {
-    "pathToReasonfmt": {
-      "title": "Path To Reasonfmt",
+    "pathToRefmt": {
+      "title": "Path To Refmt",
       "type": "string",
-      "default": "reasonfmt",
+      "default": "refmt",
       "description":
-        "Absolute path of `reasonfmt` binary - Reason syntax source formatter."
+        "Absolute path of `refmt` binary - Reason syntax source formatter."
     },
     "pathToRefmttype": {
       "title": "Path To Refmttype",
@@ -136,7 +136,7 @@ module.exports = {
         "Absolute path of `refmttype` binary - Ocaml type to Reason type formatter."
     },
     "printWidth": {
-      "title": "Default Print Width of Reasonfmt",
+      "title": "Default Print Width of Refmt",
       "type": "number",
       "default": 110,
       "description": "Default line wrapping width for pretty printing"

editorSupport/AtomReason/output_byte_debug_js/app.js
@@ -5798,10 +5798,10 @@
      _gJ_=caml_new_string_c_(str_Format_Success_cU_),
      _gE_=caml_new_string_c_("Failure!"),
      _gt_=caml_new_string_c_("\n"),
-     _gp_=caml_new_string_c_("AtomReason.pathToReasonfmt"),
+     _gp_=caml_new_string_c_("AtomReason.pathToRefmt"),
      _gq_=
       caml_new_string_c_
-       ("You must setup AtomReason.pathToReasonfmt in your Atom config"),
+       ("You must setup AtomReason.pathToRefmt in your Atom config"),
      _gr_=caml_new_string_c_("AtomReason.printWidth"),
      _gs_=caml_new_string_c_("AtomReason.printWidth must be an integer"),
      _gG_=[0,0,0],
@@ -8423,7 +8423,7 @@
         /*<</Users/chenglou/Github/Reason/editorSupport/AtomReason/src/AtomReasonFormat.re 64 11>>*/ match_k_=
          /*<</Users/chenglou/Github/Reason/editorSupport/AtomReason/src/AtomReasonFormat.re 64 11>>*/ _an_
          (_gp_);
-       /*<</Users/chenglou/Github/Reason/editorSupport/AtomReason/src/AtomReasonFormat.re 64 57>>*/ if
+       /*<</Users/chenglou/Github/Reason/editorSupport/AtomReason/src/AtomReasonFormat.re 64 53>>*/ if
        (typeof match_k_!=="number"&&0===match_k_[0])
        { /*<</Users/chenglou/Github/Reason/editorSupport/AtomReason/src/AtomReasonFormat.re 65 24>>*/ var
           /*<</Users/chenglou/Github/Reason/editorSupport/AtomReason/src/AtomReasonFormat.re 65 24>>*/ pth_m_=
@@ -8570,7 +8570,7 @@
        /*<</Users/chenglou/Github/Reason/editorSupport/AtomReason/src/AtomReasonFormat.re 61 2>>*/  /*<</Users/chenglou/Github/Reason/editorSupport/AtomReason/src/AtomReasonFormat.re 66 11>>*/ var
        _r_=
         [0,_ak_,_gq_];
-       /*<</Users/chenglou/Github/Reason/editorSupport/AtomReason/src/AtomReasonFormat.re 66 99>>*/ throw _r_ /*<</Users/chenglou/Github/Reason/editorSupport/AtomReason/src/AtomReasonFormat.re 120 31>>*/ }
+       /*<</Users/chenglou/Github/Reason/editorSupport/AtomReason/src/AtomReasonFormat.re 66 95>>*/ throw _r_ /*<</Users/chenglou/Github/Reason/editorSupport/AtomReason/src/AtomReasonFormat.re 120 31>>*/ }
      /*<</Users/chenglou/Github/Reason/editorSupport/AtomReason/src/AtomReasonFormat.re 14 61>>*/ function
      _gH_
      (editor_a_,

editorSupport/AtomReason/src/AtomReasonFormat.re
@@ -61,9 +61,9 @@ let formatImpl editor subText isInterface onComplete onFailure => {
   let stdOutLines = {contents: [||]};
   let stdErrLines = {contents: [||]};
   let fmtPath =
-    switch (Atom.Config.get "AtomReason.pathToReasonfmt") {
+    switch (Atom.Config.get "AtomReason.pathToRefmt") {
     | JsonString pth => pth
-    | _ => raise (Invalid_argument "You must setup AtomReason.pathToReasonfmt in your Atom config")
+    | _ => raise (Invalid_argument "You must setup AtomReason.pathToRefmt in your Atom config")
     };
   let printWidth =
     switch (Atom.Config.get "AtomReason.printWidth") {

commit 8f018c68c437ff5ebd9adf5cdff8a55e3daedc43

Fix gitignore to un-ignore nested dir refmt_merlin_impl.sh
Also add that file back. This is caused by
92985c45996884a8e521ab977a0056966289d0a8

.gitignore
@@ -1,6 +1,6 @@
 *.native
-reasonfmt_merlin_impl.sh
-refmt_merlin_impl.sh
+/reasonfmt_merlin_impl.sh
+/refmt_merlin_impl.sh
 *.byte
 *.docdir
 _build

src/refmt_merlin_impl.sh
@@ -1,0 +1,23 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+REFMT=""
+
+if [[ -f $DIR/refmt_impl.native ]];
+then
+    REFMT="$DIR/refmt_impl.native"
+fi
+
+if [[ -f $DIR/refmt ]];
+then
+    REFMT="$DIR/refmt"
+fi
+
+if [[ -z $REFMT  ]];
+then
+    >&2 echo "Couldn't find refmt"
+    exit 1
+fi
+
+$REFMT $@ -recoverable true -parse re
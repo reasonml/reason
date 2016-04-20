8f018c68c437ff5ebd9adf5cdff8a55e3daedc43
Cheng Lou <chenglou92@gmail.com>
2016-04-20 15:58:41 -0700

    Fix gitignore to un-ignore nested dir refmt_merlin_impl.sh
    Also add that file back. This is caused by
    92985c45996884a8e521ab977a0056966289d0a8

---
+++ src/refmt_merlin_impl.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+REFMT=""
+
+if [[ -f $DIR/refmt_impl.native ]];
+then
+    REFMT="$DIR/refmt_impl.native"
+fi
+
+if [[ -f $DIR/refmt ]];
+then
+    REFMT="$DIR/refmt"
+fi
+
+if [[ -z $REFMT  ]];
+then
+    >&2 echo "Couldn't find refmt"
+    exit 1
+fi
+
+$REFMT $@ -recoverable true -parse re
