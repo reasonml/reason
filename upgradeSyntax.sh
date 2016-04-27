fecf46e12be7567b09ebf88a82aaad1ce63ef994
Jordan Walke <jordojw@gmail.com>
2016-04-27 02:51:17 -0700

    Fix upgradeSyntax.sh to supply -is-interface-pp

--- upgradeSyntax.sh
+++ upgradeSyntax.sh
@@ -16,7 +16,7 @@ find . | grep "\.re$" | xargs -0 node -e "\
 process.argv[1].split('\n').filter(function(n){return n;}).forEach(function(filePath) { \
   var binary = require('child_process').execSync('$1 ' + filePath + ' -print binary_reason');
   var reformatted = require('child_process').execSync(
-     '$2 -use-stdin true -parse binary_reason -print re -print-width $3 | sed -e \'s/ *\$//g\'',
+     '$2 -use-stdin true -is-interface-pp false -parse binary_reason -print re -print-width $3 | sed -e \'s/ *\$//g\'',
      {input: binary}
   );
   require('fs').writeFileSync(filePath, reformatted);
