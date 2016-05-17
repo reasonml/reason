e4529a3f5ba825dbc4c9f018dbe703ccfdba448e
Yutong Pei <yutong@uber.com>
2016-05-17 14:01:48 -0700

    fix a typo in src/redoc.sh

--- src/redoc.sh
+++ src/redoc.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# redoc is a ocamldoc wrapper for producing html docs in Reason syntax
+# redoc is an ocamldoc wrapper for producing html docs in Reason syntax
 
 REASON_DIR=`ocamlfind query reason`
 
