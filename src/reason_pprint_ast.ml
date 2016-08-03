ec850268537847bc97b8d566875776baa8d1b0bb
Yunxing Dai <yunxing@cloudwalk.me>
2016-08-03 07:44:07 -0700

    Fix build

--- src/reason_pprint_ast.ml
+++ src/reason_pprint_ast.ml
@@ -1367,35 +1367,6 @@ let smallestLeadingSpaces strs =
   in
   smallestLeadingSpaces 99999 strs
 
-let string_after s n = String.sub s n (String.length s - n)
-
-let formatItemComment (str, commLoc, physCommLoc) =
-  let commLines = Re_str.split_delim (Re_str.regexp "\n") ("/*" ^ str ^ "*/") in
-  match commLines with
-  | [] -> easyAtom ""
-  | [hd] ->
-    makeEasyList ~inline:(true, true) ~postSpace:true ~preSpace:true ~indent:0 ~break:IfNeed [easyAtom hd]
-  | zero::one::tl ->
-    let lineZero = List.nth commLines 0 in
-    let lineOne = List.nth commLines 1 in
-    let hasMeaningfulContentOnLineZero = lineZeroHasMeaningfulContent lineZero in
-    let attemptRemoveCount = (smallestLeadingSpaces (one::tl)) in
-    (* If only OCaml, had a left-pad module. *)
-    let leftPad =
-      if beginsWithStar lineOne then 1
-      else (if hasMeaningfulContentOnLineZero then spaceBeforeMeaningfulContent lineZero else 1)
-    in
-    let padNonOpeningLine s =
-      let numLeadingSpaceForThisLine = numLeadingSpace s in
-      if String.length s == 0 then ""
-      else (String.make leftPad ' ') ^
-        (string_after s (min attemptRemoveCount numLeadingSpaceForThisLine)) in
-    let lines = zero :: List.map padNonOpeningLine (one::tl) in
-    (* Use the Str module for regex splitting *)
-    makeEasyList ~inline:(true, true) ~indent:0 ~break:Always_rec (List.map easyAtom lines)
-
-let formatComments comms = List.map formatItemComment comms
-
 let convertIsListyToIsSequencey isListyImpl =
   let rec isSequencey layoutNode = match layoutNode with
     | SourceMap (_, subLayoutNode) -> isSequencey subLayoutNode
@@ -1419,6 +1390,8 @@ let insertBlankLines n term =
     term
   else
     makeList ~inline:(true, true) ~indent:0 ~break:Always_rec (Array.to_list (Array.make n (atom "")) @ [term])
+
+let string_after s n = String.sub s n (String.length s - n)
 
 let wrapComment txt =
   ("/*" ^ txt ^ "*/")
