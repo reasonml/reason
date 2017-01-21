60e5e99688a780ae9132559854b73401c683c4e1
Jordan Walke <jordojw@gmail.com>
2017-01-20 20:18:15 -0800

    Version 1.6.0

--- package.ml
+++ package.ml
@@ -1,4 +1,4 @@
 
-let version = "1.5.0"
-let git_version = "4da862e8f2c8e69c6a726153fe3b25d52fe011e2"
-let git_short_version = "4da862e"
+let version = "1.6.0"
+let git_version = "0bc6ccf3d6890adb09b31e6074db1cb7d652988c"
+let git_short_version = "0bc6ccf"
