ebf834c27bb1d11fd2d416458fcce573a6dace3a
Max Bernstein <emacs@fb.com>
2017-01-23 14:00:02 -0800

    Make spacing more consistent

--- src/refmt_args.ml
+++ src/refmt_args.ml
@@ -1,79 +1,79 @@
 open Cmdliner
 
 let interface =
-    let doc = "parse AST as an interface" in
-    Arg.(value & opt (bool) false & info ["i"; "interface"] ~doc)
+  let doc = "parse AST as an interface" in
+  Arg.(value & opt (bool) false & info ["i"; "interface"] ~doc)
 
 let recoverable =
-    let doc = "enable recoverable parser" in
-    Arg.(value & flag & info ["r"; "recoverable"] ~doc)
+  let doc = "enable recoverable parser" in
+  Arg.(value & flag & info ["r"; "recoverable"] ~doc)
 
 let explicit_arity =
-    let doc =
-        "If a constructor's argument is a tuple, always interpret it as \
-        multiple arguments"
-    in
-    Arg.(value & flag & info ["e"; "assume-explicit-arity"] ~doc)
+  let doc =
+    "If a constructor's argument is a tuple, always interpret it as \
+     multiple arguments"
+  in
+  Arg.(value & flag & info ["e"; "assume-explicit-arity"] ~doc)
 
 let parse_ast =
-    let docv = "FORM" in
-    let doc = "print AST in FORM, which is one of: (ml | re | \
-                  binary (default - for compiler input) | \
-                  binary_reason (for interchange between Reason versions))"
-    in
-    let opts = Arg.enum ["ml", `ML; "re", `Reason;
-                         "binary_reason", `BinaryReason]
-    in
-    Arg.(value & opt (some opts) None & info ["parse"] ~docv ~doc)
+  let docv = "FORM" in
+  let doc = "print AST in FORM, which is one of: (ml | re | \
+             binary (default - for compiler input) | \
+             binary_reason (for interchange between Reason versions))"
+  in
+  let opts = Arg.enum ["ml", `ML; "re", `Reason;
+                       "binary_reason", `BinaryReason]
+  in
+  Arg.(value & opt (some opts) None & info ["parse"] ~docv ~doc)
 
 let print =
-    let docv = "FORM" in
-    let doc = "print AST in FORM, which is one of: (ml | re | \
-                  binary (default - for compiler input) | \
-                  binary_reason (for interchange between Reason versions) | \
-                  ast (print human readable AST directly) | none)"
-    in
-    let opts = Arg.enum ["ml", `ML; "re", `Reason; "binary", `Binary;
-                         "binary_reason", `BinaryReason; "ast", `AST;
-                         "none", `None]
-    in
-    Arg.(value & opt (some opts) None & info ["p"; "print"] ~docv ~doc)
+  let docv = "FORM" in
+  let doc = "print AST in FORM, which is one of: (ml | re | \
+             binary (default - for compiler input) | \
+             binary_reason (for interchange between Reason versions) | \
+             ast (print human readable AST directly) | none)"
+  in
+  let opts = Arg.enum ["ml", `ML; "re", `Reason; "binary", `Binary;
+                       "binary_reason", `BinaryReason; "ast", `AST;
+                       "none", `None]
+  in
+  Arg.(value & opt (some opts) None & info ["p"; "print"] ~docv ~doc)
 
 let print_width =
-    let docv = "COLS" in
-    let doc = "wrapping width for printing the AST" in
-    Arg.(value & opt (int) (100) & info ["w"; "print-width"] ~docv ~doc)
+  let docv = "COLS" in
+  let doc = "wrapping width for printing the AST" in
+  Arg.(value & opt (int) (100) & info ["w"; "print-width"] ~docv ~doc)
 
 let heuristics_file =
-    let doc =
-        "load path as a heuristics file to specify which constructors are defined with \
+  let doc =
+    "load path as a heuristics file to specify which constructors are defined with \
      multi-arguments. Mostly used in removing [@implicit_arity] introduced from \
      OCaml conversion.\n\t\texample.txt:\n\t\tConstructor1\n\t\tConstructor2"
-    in
-    Arg.(value & opt (some file) None & info ["h"; "heuristics-file"] ~doc)
+  in
+  Arg.(value & opt (some file) None & info ["h"; "heuristics-file"] ~doc)
 
 let output =
-    let docv = "FILENAME" in
-    let doc = "target file for output; default [stdout]" in
-    Arg.(value & opt (some string) None & info ["o"; "output"] ~docv ~doc)
+  let docv = "FILENAME" in
+  let doc = "target file for output; default [stdout]" in
+  Arg.(value & opt (some string) None & info ["o"; "output"] ~docv ~doc)
 
 let in_place =
-    let doc = "reformat a file in-place" in
-    Arg.(value & flag & info ["in-place"] ~doc)
+  let doc = "reformat a file in-place" in
+  Arg.(value & flag & info ["in-place"] ~doc)
 
 let input =
-    let docv = "FILENAME" in
-    let doc = "input file" in
-    Arg.(value & pos ~rev:true 0 (some file) None & info [] ~docv ~doc)
+  let docv = "FILENAME" in
+  let doc = "input file" in
+  Arg.(value & pos ~rev:true 0 (some file) None & info [] ~docv ~doc)
 
 (* DEPRECATED *)
 
 let is_interface_pp =
-    let doc = "is-interface-pp is DEPRECATED; use -i or --interface instead" in
-    Arg.(value & flag & info ["is-interface-pp"] ~doc)
+  let doc = "is-interface-pp is DEPRECATED; use -i or --interface instead" in
+  Arg.(value & flag & info ["is-interface-pp"] ~doc)
 
 let use_stdin =
-    let doc = "use-stdin is DEPRECATED; usage is assumed if not specifying a \
-    filename"
-    in
-    Arg.(value & flag & info ["use-stdin"] ~doc)
+  let doc = "use-stdin is DEPRECATED; usage is assumed if not specifying a \
+             filename"
+  in
+  Arg.(value & flag & info ["use-stdin"] ~doc)
--- src/refmt_impl.ml
+++ src/refmt_impl.ml
@@ -23,24 +23,24 @@ let version = "Reason " ^ Package.version ^ " @ " ^ Package.git_short_version
 
 
 let refmt
-      interface
-      is_recoverable
-      explicit_arity
-      parse_ast
-      print
-      print_width
-      h_file
-      output_file
-      in_place
-      input_file
-      is_interface_pp
-      use_stdin
+    interface
+    is_recoverable
+    explicit_arity
+    parse_ast
+    print
+    print_width
+    h_file
+    output_file
+    in_place
+    input_file
+    is_interface_pp
+    use_stdin
   =
   let () =
-      if is_interface_pp then
-          raise (Invalid_config "--is-interface-pp is deprecated.")
-      else if use_stdin then
-          raise (Invalid_config "--use-stdin is deprecated.")
+    if is_interface_pp then
+      raise (Invalid_config "--is-interface-pp is deprecated.")
+    else if use_stdin then
+      raise (Invalid_config "--use-stdin is deprecated.")
   in
   let (use_stdin, input_file) = match input_file with
     | Some name -> (false, name)
@@ -56,8 +56,8 @@ let refmt
       | None -> false
     in
     if input_file = "" && not (has_parse && has_print) then
-        raise (Invalid_config "Need an input file, parse mode, and print \
-        mode. Modes can be auto-detected based on filename.")
+      raise (Invalid_config "Need an input file, parse mode, and print \
+                             mode. Modes can be auto-detected based on filename.")
   in
   Reason_config.configure ~r:is_recoverable;
   Location.input_name := input_file;
@@ -74,13 +74,13 @@ let refmt
     | None -> false
   in
   let output_file =
-      if in_place then
-          if use_stdin then
-              raise (Invalid_config "Cannot write in place to stdin.")
-          else if writing_to_file then
-              raise (Invalid_config "Cannot specify --output and --in-place.")
-          else Some input_file
-      else output_file
+    if in_place then
+      if use_stdin then
+        raise (Invalid_config "Cannot write in place to stdin.")
+      else if writing_to_file then
+        raise (Invalid_config "Cannot specify --output and --in-place.")
+      else Some input_file
+    else output_file
   in
   let (module Printer : Printer_maker.PRINTER) =
     if interface then (module Reason_interface_printer)
@@ -99,48 +99,46 @@ let refmt
   let output_formatter = Format.formatter_of_out_channel output_chan in
   let thePrinter = Printer.makePrinter print input_file parsedAsML output_chan output_formatter in
   (
-      thePrinter ast;
-      (* Also closes all open boxes. *)
-      Format.pp_print_flush output_formatter ();
-      flush output_chan;
-      Printer_maker.close_output_file output_file output_chan;
-      `Ok ()
+    thePrinter ast;
+    (* Also closes all open boxes. *)
+    Format.pp_print_flush output_formatter ();
+    flush output_chan;
+    Printer_maker.close_output_file output_file output_chan;
+    `Ok ()
   )
 
 (* TODO: gross solution. fix. *)
 let refmt_wrapper a b c d e f g h i j k l =
-    try
-        refmt a b c d e f g h i j k l
-    with
-    | Invalid_config msg -> `Error (true, msg)
-    | exn ->
-            Location.report_exception Format.err_formatter exn;
-            exit 1
+  try
+    refmt a b c d e f g h i j k l
+  with
+  | Invalid_config msg -> `Error (true, msg)
+  | exn ->
+    Location.report_exception Format.err_formatter exn;
+    exit 1
 
 let top_level_info =
   let doc = "Meta language utility" in
-  let man = [`S "DESCRIPTION";
-             `P "refmt is a parser and pretty-printer"]
-  in
+  let man = [`S "DESCRIPTION"; `P "refmt is a parser and pretty-printer"] in
   Term.info "refmt" ~version ~doc ~man
 
 let refmt_t =
-    let open Term in
-    let open Refmt_args in
-    const refmt_wrapper $ interface
-                        $ recoverable
-                        $ explicit_arity
-                        $ parse_ast
-                        $ print
-                        $ print_width
-                        $ heuristics_file
-                        $ output
-                        $ in_place
-                        $ input
-                        $ is_interface_pp
-                        $ use_stdin
+  let open Term in
+  let open Refmt_args in
+  const refmt_wrapper $ interface
+                      $ recoverable
+                      $ explicit_arity
+                      $ parse_ast
+                      $ print
+                      $ print_width
+                      $ heuristics_file
+                      $ output
+                      $ in_place
+                      $ input
+                      $ is_interface_pp
+                      $ use_stdin
 
 let () =
-    match Term.eval ((Term.ret refmt_t), top_level_info) with
-    | `Error s -> exit 1
-    | _ -> exit 0
+  match Term.eval ((Term.ret refmt_t), top_level_info) with
+  | `Error s -> exit 1
+  | _ -> exit 0
