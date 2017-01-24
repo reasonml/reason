4ae6a1739c6d22d5daccd039e9a1207431fa6898
Max Bernstein <emacs@fb.com>
2017-01-24 11:55:28 -0800

    Bump version because we added a dependency

--- package.ml
+++ package.ml
@@ -1,4 +1,4 @@
 
-let version = "1.4.0"
-let git_version = "1e566af14558693f71d89f1a1c4d9e2e4e98b579"
-let git_short_version = "1e566af"
+let version = "1.5.0"
+let git_version = "f663d4cc19ebd5829c0521b41ce397dee339bb75"
+let git_short_version = "f663d4c"
