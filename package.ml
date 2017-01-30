5860438d427aaa1df6f74dd459a3c1f24b36d56a
Yunxing Dai <yunxing@fb.com>
2017-01-30 15:56:38 -0800

    Version 1.7.0

--- package.ml
+++ package.ml
@@ -1,4 +1,4 @@
 
 let version = "1.7.0"
-let git_version = "264416b4fe1c41d0ade363a2c6cc0afdf7e8dcd6"
-let git_short_version = "264416b"
+let git_version = "1daf3899bfffc998acb42967bb1c0fe30d965078"
+let git_short_version = "1daf389"
