2f5fbf48abc32149006052788f042569e48fe43d
Yunxing Dai <yunxing@fb.com>
2017-02-07 12:10:10 -0800

    Version 1.7.4

--- package.ml
+++ package.ml
@@ -1,4 +1,4 @@
 
-let version = "1.7.3"
-let git_version = "a444d34118409ee70fa78cbcc17b4e2544f920c3"
-let git_short_version = "a444d34"
+let version = "1.7.4"
+let git_version = "73f574df44f919779b2ab92643c1950c5cfb7f00"
+let git_short_version = "73f574d"
