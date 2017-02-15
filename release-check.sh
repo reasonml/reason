4c4c777d06b0294ac2049ce69bffa8ec7bea281c
Max Bernstein <emacs@fb.com>
2017-02-15 13:05:44 -0800

    Add warning about GitHub and npm

--- release-check.sh
+++ release-check.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 RANDSTR=`head -c2 </dev/urandom|xxd -p -u`
-read -p "Do you want to release? If so, please type '${RANDSTR}' (no quotes): " inp
+echo "Do you want to release? This *WILL* publish to GitHub AND npm!!!"
+read -p "If so, please type '${RANDSTR}' (no quotes): " inp
 if [ "$inp" = "$RANDSTR" ]; then
     echo "Preparing to release..."
     exit 0
