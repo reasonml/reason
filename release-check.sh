5c61b81331cbab878d90af485f74d54204a366a0
Max Bernstein <emacs@fb.com>
2017-02-21 12:01:41 -0800

    Ensure that current HEAD is on upstream master
    Fix #1063

--- release-check.sh
+++ release-check.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
-RANDSTR=`head -c2 </dev/urandom|xxd -p -u`
+die () { echo "${1}"; exit 1; }
+
+# Check that the current HEAD is on origin/master.
+HEAD=`git rev-parse --verify HEAD`
+MASTERURL="https://api.github.com/repos/facebook/reason/git/refs/heads/master"
+HEADMSG="Current HEAD is not on upstream master. This is a requirement before releasing.
+If you are sure it is, try switching branches to master and pulling changes."
+echo "Checking HEAD against upstream master..."
+curl --silent $MASTERURL | grep "sha" | grep $HEAD || die "$HEADMSG"
+
+# Confirm that the user actually means to release.
+RANDSTR=`head -c2 </dev/urandom | xxd -plain -u` # -u is uppercase
 echo "Do you want to release? This *WILL* publish to GitHub AND npm!!!"
 read -p "If so, please type '${RANDSTR}' (no quotes): " inp
 if [ "$inp" = "$RANDSTR" ]; then
     echo "Preparing to release..."
     exit 0
 else
-    echo "Not releasing."
-    exit 1
+    die "Not releasing."
 fi
