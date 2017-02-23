0d6986d5e90291c5da7d0bbcca5d4c3e11776701
Max Bernstein <emacs@fb.com>
2017-02-23 11:48:53 -0800

    Bring tests up to date

--- formatTest/printerTests/expected_output/rectypetest.ml
+++ formatTest/printerTests/expected_output/rectypetest.ml
@@ -1,20 +1,20 @@
 module Ppx_deriving_runtime =
   struct
-    type nonrec int = int[@@nonrec ]
-    type nonrec char = char[@@nonrec ]
-    type nonrec string = string[@@nonrec ]
-    type nonrec float = float[@@nonrec ]
-    type nonrec bool = bool[@@nonrec ]
-    type nonrec unit = unit[@@nonrec ]
-    type nonrec exn = exn[@@nonrec ]
-    type nonrec 'a array = 'a array[@@nonrec ]
-    type nonrec 'a list = 'a list[@@nonrec ]
-    type nonrec 'a option = 'a option[@@nonrec ]
-    type nonrec nativeint = nativeint[@@nonrec ]
-    type nonrec int32 = int32[@@nonrec ]
-    type nonrec int64 = int64[@@nonrec ]
-    type nonrec 'a lazy_t = 'a lazy_t[@@nonrec ]
-    type nonrec bytes = bytes[@@nonrec ]
+    type nonrec int = int
+    type nonrec char = char
+    type nonrec string = string
+    type nonrec float = float
+    type nonrec bool = bool
+    type nonrec unit = unit
+    type nonrec exn = exn
+    type nonrec 'a array = 'a array
+    type nonrec 'a list = 'a list
+    type nonrec 'a option = 'a option
+    type nonrec nativeint = nativeint
+    type nonrec int32 = int32
+    type nonrec int64 = int64
+    type nonrec 'a lazy_t = 'a lazy_t
+    type nonrec bytes = bytes
     module Pervasives = Pervasives
     module Char = Char
     module String = String
@@ -37,12 +37,12 @@ module Ppx_deriving_runtime =
     include Pervasives
   end
 type mytype =
-  | Int of hello
-  | Float of int list
-  | String
+  | Int of hello 
+  | Float of int list 
+  | String 
 and hello = string
-let rec pp_mytype : Format.formatter -> mytype -> Ppx_deriving_runtime.unit=
-  let __0 () = pp_hello in
+let rec pp_mytype : Format.formatter -> mytype -> Ppx_deriving_runtime.unit =
+  let __0 () = pp_hello  in
   ((let open! Ppx_deriving_runtime in
       fun fmt  ->
         function
@@ -63,14 +63,18 @@ let rec pp_mytype : Format.formatter -> mytype -> Ppx_deriving_runtime.unit=
                            true) false x);
                  Format.fprintf fmt "@,]@]")) a0;
              Format.fprintf fmt "@])")
-        | String  -> Format.pp_print_string fmt "String")[@ocaml.warning
-                                                           "-A"])
-and show_mytype : mytype -> Ppx_deriving_runtime.string=
+        | String  -> Format.pp_print_string fmt "String")
+    [@ocaml.warning "-A"])
+
+and show_mytype : mytype -> Ppx_deriving_runtime.string =
   fun x  -> Format.asprintf "%a" pp_mytype x
+
 and (pp_hello : Format.formatter -> hello -> Ppx_deriving_runtime.unit) =
   ((let open! Ppx_deriving_runtime in fun fmt  -> Format.fprintf fmt "%S")
   [@ocaml.warning "-A"])
-and show_hello : hello -> Ppx_deriving_runtime.string=
+
+and show_hello : hello -> Ppx_deriving_runtime.string =
   fun x  -> Format.asprintf "%a" pp_hello x
-let () = print_endline (show_mytype ((Int ("five"))[@explicit_arity ]))
-let _ = print_endline (show_hello "aloha")
+
+let () = print_endline (show_mytype ((Int ("five"))[@explicit_arity ])) 
+;;print_endline (show_hello "aloha")
