c067a8f3deae67d53894e0f5307179b36b489b32
Max Bernstein <emacs@fb.com>
2017-02-23 14:04:12 -0800

    Add version-specific tests

--- test-with-version.sh
+++ test-with-version.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-OCAML_VERSION="${1}"
+export OCAML_VERSION="${1}"
 
 make clean
-opam switch "$OCAML_VERSION"
+opam switch "${OCAML_VERSION}"
+eval `opam config env`
 opam update
 opam pin add -y reason .
-eval `opam config env`
 make test
