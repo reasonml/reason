a958fe700891c64d887cc366c6a156bed38187d6
Cheng Lou <chenglou92@gmail.com>
2017-03-01 16:01:05 -0800

    Polish bspacking instructions

--- bspacks/reason_bspack.sh
+++ bspacks/reason_bspack.sh
@@ -18,7 +18,15 @@ rm -f refmt_main.ml
 rm -f reactjs_ppx.ml
 
 echo "* Packing refmt"
-./bspack_source/bspack.exe -I `menhir --suggest-menhirLib` -bs-main Refmt_impl -I ../_build/src -I ../vendor/cmdliner -I ../vendor/easy_format/ -I ../vendor/ppx_deriving/ -o refmt_main.ml
+./bspack_source/bspack.exe -I `menhir --suggest-menhirLib` -bs-main Refmt_impl -I ../_build/src -I ../_build -I ../vendor/cmdliner -I ../vendor/easy_format/ -I ../vendor/ppx_deriving/ -o refmt_main.ml
 
 echo "* Packing reactjs_ppx"
 ./bspack_source/bspack.exe -I `menhir --suggest-menhirLib` -bs-main Reactjs_jsx_ppx -I ../_build/src -I ../vendor/cmdliner -I ../vendor/easy_format/ -I ../vendor/ppx_deriving/ -o reactjs_ppx.ml
+
+
+# to compile into binaries: https://github.com/bloomberg/bucklescript/blob/b515a95c5a5740d59cf7eaa9c4fd46863598197f/jscomp/bin/Makefile#L29-L33
+
+# ocamlopt.opt -I +compiler-libs ocamlcommon.cmxa refmt_main.mli refmt_main.ml -o refmt.exe
+# ocamlopt.opt -I +compiler-libs ocamlcommon.cmxa reactjs_ppx.mli reactjs_ppx.ml -o reactjs_jsx_ppx.exe
+
+# you only need to compile them to test that the bundling worked. Otherwise, just copy paste the *.ml files to BuckleScript and submit a PR. Their makefile will take care of compiling to binaries.
