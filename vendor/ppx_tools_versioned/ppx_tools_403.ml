2b39d8ee75d247bc2bd6d879bf7644e8d72ff577
Max Bernstein <emacs@fb.com>
2017-03-13 14:18:20 -0700

    Un-vendor ppx_tools_versioned

--- vendor/ppx_tools_versioned/ppx_tools_403.ml
+++
@@ -1,2 +0,0 @@
-module Ast_convenience = Ast_convenience_403
-module Ast_mapper_class = Ast_mapper_class_403
--- vendor/ppx_tools_versioned/ppx_tools_405.ml
+++
@@ -1,2 +0,0 @@
-module Ast_convenience = Ast_convenience_405
-module Ast_mapper_class = Ast_mapper_class_405
