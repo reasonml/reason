587a45a8dbf05c823639f8759b5b601ca6b80a61
Max Bernstein <emacs@fb.com>
2017-03-16 09:50:32 -0700

    Update version in subst package.json

--- package.ml
+++ package.ml
@@ -1,4 +1,4 @@
 
 let version = "1.10.2"
-let git_version = "2b39d8ee75d247bc2bd6d879bf7644e8d72ff577"
-let git_short_version = "2b39d8e"
+let git_version = "e3576ae51a73647b7f1e0c73286e91d4d3f35692"
+let git_short_version = "e3576ae"
