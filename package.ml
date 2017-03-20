09d27adac25f13ee4f132fe6c5e192fdfc31a81b
Max Bernstein <emacs@fb.com>
2017-03-20 11:40:40 -0700

    Fix package.json for including reason-parser
    Now builds with npm

--- package.ml
+++ package.ml
@@ -1,4 +1,4 @@
 
 let version = "1.12.0"
-let git_version = "6534c3343418b9803ab97bb441585d7e00982165"
-let git_short_version = "6534c33"
+let git_version = "052aba0605baf088423f5407b306ed935a5e1a52"
+let git_short_version = "052aba0"
