fe4edf639bc65ec7d941a16542c942c00efecc24
Cheng Lou <chenglou92@gmail.com>
2017-03-20 18:40:50 -0700

    Including more of the recent deps in bspack workflow

--- bspacks/reason_bspack.sh
+++ bspacks/reason_bspack.sh
@@ -12,10 +12,30 @@ rm -f refmt_main.ml
 rm -f reactjs_ppx.ml
 
 echo "* Packing refmt"
-./node_modules/bs-platform/bin/bspack.exe -I `menhir --suggest-menhirLib` -bs-main Refmt_impl -I ../_build/src -I ../_build -I ../vendor/cmdliner -I ../vendor/easy_format -I ../vendor/ppx_deriving -I ../node_modules/result-actual -o refmt_main.ml
+./node_modules/bs-platform/bin/bspack.exe \
+  -I `menhir --suggest-menhirLib` -bs-main Refmt_impl \
+  -I ../_build/src \
+  -I ../_build \
+  -I ../vendor/cmdliner \
+  -I ../vendor/easy_format \
+  -I ../vendor/ppx_deriving \
+  -I `ocamlfind query ppx_tools_versioned` \
+  -I `ocamlfind query result` \
+  -I ../node_modules/ocaml-migrate-parsetree-actual/_build/default/src \
+  -o refmt_main.ml
+  # for Fred: this doesn't work
+  # -I `ocamlfind query ocaml-migrate-parsetree` \
 
 echo "* Packing reactjs_ppx"
-./node_modules/bs-platform/bin/bspack.exe -I `menhir --suggest-menhirLib` -bs-main Reactjs_jsx_ppx -I ../_build/src -I ../vendor/cmdliner -I ../vendor/easy_format/ -I ../vendor/ppx_deriving/ -I ../node_modules/result-actual -o reactjs_ppx.ml
+./node_modules/bs-platform/bin/bspack.exe \
+  -I `menhir --suggest-menhirLib` -bs-main Reactjs_jsx_ppx \
+  -I ../_build/src \
+  -I ../vendor/cmdliner \
+  -I ../vendor/easy_format/ \
+  -I ../vendor/ppx_deriving/ \
+  -I `ocamlfind query result` \
+  -I ../node_modules/ocaml-migrate-parsetree-actual/_build/default/src \
+  -o reactjs_ppx.ml
 
 # to compile into binaries: https://github.com/bloomberg/bucklescript/blob/b515a95c5a5740d59cf7eaa9c4fd46863598197f/jscomp/bin/Makefile#L29-L33
 # you only need to compile these to test that the bundling worked. Otherwise,
