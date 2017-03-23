c6f0f4a38e4929b44bd13b05837d61de1537d63f
Yunxing Dai <yunxing@fb.com>
2017-03-22 17:38:28 -0700

    Version 1.13.0

--- package.ml
+++ package.ml
@@ -1,4 +1,4 @@
 
-let version = "1.12.0"
-let git_version = "052aba0605baf088423f5407b306ed935a5e1a52"
-let git_short_version = "052aba0"
+let version = "1.13.0"
+let git_version = "dbe5d86a16543291cc1fa969851130b93f5a4649"
+let git_short_version = "dbe5d86"
