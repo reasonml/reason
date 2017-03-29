000ab80359558eaf979214b0dce95869646c6924
Max Bernstein <emacs@fb.com>
2017-03-29 11:51:14 -0700

    Version 1.13.3

--- package.ml
+++ package.ml
@@ -1,4 +1,4 @@
 
-let version = "1.13.2"
-let git_version = "713f570e554a6caf401580441fbeae280bff8daf"
-let git_short_version = "713f570"
+let version = "1.13.3"
+let git_version = "662f602a0b070e8bd632d891b391213cd737b578"
+let git_short_version = "662f602"
