b5971d94150a616d50ff2ab07e528078b28a3e0d
Frédéric Bour <frederic.bour@lakaban.net>
2017-06-15 17:14:33 +0200

    REMOVE ME update reason-parser/test

--- reason-parser/src/test.ml
+++ reason-parser/src/test.ml
@@ -1,9 +1,17 @@
+
 let () =
+  let fname = Sys.argv.(1) in
+  let lexbuf = Lexing.from_channel (open_in fname) in
   try
-    let lexbuf = Lexing.from_channel stdin in
-    let x = Reason_toolchain.JS.canonical_implementation_with_comments lexbuf in
-    Reason_toolchain.JS.print_canonical_implementation_with_comments
-      Format.std_formatter x;
+    if Filename.check_suffix fname ".re" then (
+      let x = Reason_toolchain.JS.canonical_implementation_with_comments lexbuf in
+      Reason_toolchain.JS.print_canonical_implementation_with_comments
+        Format.std_formatter x;
+    ) else (
+      let x = Reason_toolchain.JS.canonical_interface_with_comments lexbuf in
+      Reason_toolchain.JS.print_canonical_interface_with_comments
+        Format.std_formatter x;
+    );
     Format.pp_flush_formatter Format.std_formatter
   with Syntax_util.Error (loc, Syntax_util.Syntax_error msg) ->
     let {Lexing. pos_lnum; pos_bol; pos_cnum} = loc.Location.loc_start in
