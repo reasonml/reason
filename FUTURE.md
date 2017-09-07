c11247f9ef2724203ff6121eda952b5dbe4137b2
Cheng Lou <chenglou92@gmail.com>
2017-09-06 18:38:33 -0700

    Remove old reactjs_jsx_ppx (#1391)
    cc @bobzhang

--- FUTURE.md
+++ FUTURE.md
@@ -7,5 +7,6 @@
 TODO: re-enable rtop integration tests in makefile (./miscTests/rtopIntegrationTest.sh) before the release
 
 - Remove unused binaries: `reup`, etc.
+- Remove the old `reactjs_jsx_ppx.ml`. You've all been on `reactjs_jsx_ppx_2.ml` for a long time now.
 - Remove `--add-printers` from `refmt`. The feature wasn't stable enough; we'll find a better way soon. Sorry about that!
 - New syntax based on (but not entirely) #1299
--- pkg/build.ml
+++ pkg/build.ml
@@ -56,7 +56,6 @@ let () =
     Pkg.bin ~auto:false "src/rtop_init.ml" ~dst:"rtop_init.ml";
     Pkg.bin ~auto:false "_reasonbuild/_build/myocamlbuild" ~dst:"rebuild";
     Pkg.bin "src/reason_format_type" ~dst:"refmttype";
-    Pkg.bin "src/reactjs_jsx_ppx" ~dst:"reactjs_jsx_ppx";
     Pkg.bin "src/reactjs_jsx_ppx_2" ~dst:"reactjs_jsx_ppx_2";
     Pkg.bin "src/ppx_react" ~dst:"ppx_react";
     Pkg.lib ~exts:(Exts.exts [ ".cmo"; ".cmx";".cmi"; ".cmt";".mli"]) "src/reason_parser";
