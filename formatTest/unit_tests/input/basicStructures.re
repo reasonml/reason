reasonml/reason

commit 2c4ee90daacd18144869ab42404a8ee281a6e1cf

Record punning for value & pattern for fields with module prefix (#1456)
* Record punning for value & pattern for fields with module prefix

Aka `{M.x: x, y: 1}` puns to `{M.x, y: 1}`.

Rather positive the expectation isn't that it's a shorthand for `{M.x:
M.x, y: 1}`...

Fixes #1431

* Tweaks

src/reason_pprint_ast.ml
@@ -3290,7 +3290,9 @@ class printer  ()= object(self:'self)
           | Ppat_record (l, closed) ->
               let longident_x_pattern (li, p) =
                 match (li, p.ppat_desc) with
-                  | ({txt=Lident s}, Ppat_var {txt}) when s = txt ->
+                  | ({txt = ident}, Ppat_var {txt}) when Longident.last ident = txt ->
+                    (* record field punning when destructuring. {x: x, y: y} becomes {x, y} *)
+                    (* works with module prefix too: {MyModule.x: x, y: y} becomes {MyModule.x, y} *)
                       self#longident_loc li
                   | _ ->
                       label ~space:true (makeList [self#longident_loc li; atom ":"]) (self#pattern p)
@@ -4960,9 +4962,10 @@ class printer  ()= object(self:'self)
         loc_end = e.pexp_loc.loc_end;
         loc_ghost = false;
       } in
-      let theRow = match e.pexp_desc with
-        (* Punning *)
-        |  Pexp_ident {txt} when li.txt = txt && shouldPun && allowPunning ->
+      let theRow = match (e.pexp_desc, shouldPun, allowPunning) with
+        (* record value punning. Turns {foo: foo, bar: 1} into {foo, bar: 1} *)
+        (* also turns {Foo.bar: bar, baz: 1} into {Foo.bar, baz: 1} *)
+        | (Pexp_ident {txt = ident}, true, true) when Longident.last li.txt = Longident.last ident ->
           makeList (maybeQuoteFirstElem li (if appendComma then [comma] else []))
         | _ ->
           let (sweet, argsList, return) = self#curriedPatternsAndReturnVal e in
@@ -4994,7 +4997,7 @@ class printer  ()= object(self:'self)
     let allRows = match eo with
       | None -> (
         match l with
-          (* No punning (or comma) for records with only a single field. *)
+          (* No punning (or comma) for records with only a single field. It's ambiguous with an expression in a scope *)
           (* See comment in parser.mly for lbl_expr_list_with_at_least_one_non_punned_field *)
           | [hd] -> [makeRow hd false false]
           | _ -> getRows l

formatTest/unit_tests/expected_output/basicStructures.re
@@ -691,6 +691,11 @@ type component2 = {
   updater: unit
 };
 
+type component3 = {
+  props: M.props,
+  state
+};
+
 type mutableComponent = {mutable props};
 
 type mutabeleComponent2 = {
@@ -708,6 +713,30 @@ type description('props) = {
 /* Don't pun types from other modules */
 module Foo = {
   type bar = {foo: Baz.foo};
+};
+
+/* record value punning */
+let props = {title: "hi"};
+
+/* no punning available for a single field. Can't tell the difference with a scope + expression */
+let componentA = {props: props};
+
+/* pun for real */
+let componentB = {props, state: ()};
+
+/* pun fields with module prefix too */
+let foo = {Foo.foo: foo};
+
+let bar = {Foo.foo, bar: 1};
+
+let bar = {bar: 1, Foo.foo};
+
+let bar = {Foo.foo, Bar.bar};
+
+({M.x, y}) => 1;
+
+switch (foo) {
+| {y: 1, M.x} => 2
 };
 
 /* Requested in #566 */

formatTest/unit_tests/input/basicStructures.re
@@ -562,6 +562,8 @@ type component = {props};
 
 type component2 = {props, state, updater: unit,};
 
+type component3 = {props: M.props, state};
+
 type mutableComponent = {mutable props};
 
 type mutabeleComponent2 = {mutable props, mutable state, style: int,};
@@ -575,6 +577,25 @@ type description('props) = {
 /* Don't pun types from other modules */
 module Foo = {
   type bar = {foo: Baz.foo};
+};
+
+/* record value punning */
+
+let props = {title: "hi"};
+/* no punning available for a single field. Can't tell the difference with a scope + expression */
+let componentA = {props: props};
+/* pun for real */
+let componentB = {props: props, state: ()};
+/* pun fields with module prefix too */
+let foo = {Foo.foo: foo};
+let bar = {Foo.foo: foo, bar: 1};
+let bar = {bar: 1, Foo.foo: foo};
+let bar = {Foo.foo: foo, Bar.bar: bar};
+
+fun ({M.x: x, y: y}) => 1;
+
+switch (foo) {
+| {y: 1, M.x: x} => 2
 };
 
 /* Requested in #566 */

commit 67f66127e1ecc1059bd9c33299a634ae25576c5d

Fixes #1508
Caused by #1456

src/reason_pprint_ast.ml
@@ -4998,7 +4998,8 @@ class printer  ()= object(self:'self)
       let theRow = match (e.pexp_desc, shouldPun, allowPunning) with
         (* record value punning. Turns {foo: foo, bar: 1} into {foo, bar: 1} *)
         (* also turns {Foo.bar: bar, baz: 1} into {Foo.bar, baz: 1} *)
-        | (Pexp_ident {txt = ident}, true, true) when Longident.last li.txt = Longident.last ident ->
+        (* don't turn {bar: Foo.bar, baz: 1} into {bar, baz: 1}, naturally *)
+        | (Pexp_ident {txt = Lident value}, true, true) when Longident.last li.txt = value ->
           makeList (maybeQuoteFirstElem li (if appendComma then [comma] else []))
         | _ ->
           let (sweet, argsList, return) = self#curriedPatternsAndReturnVal e in

formatTest/unit_tests/expected_output/basicStructures.re
@@ -731,6 +731,19 @@ module Foo = {
   type bar = {foo: Baz.foo};
 };
 
+/* Don't pun field names that aren't "simple" */
+type foo = {
+  bar: Baz.bar,
+  qux,
+  fooo: Fooo.fooo
+};
+
+let moreFoo = {
+  bar: Baz.bar,
+  qux,
+  fooo: Fooo.fooo
+};
+
 /* record value punning */
 let props = {title: "hi"};
 

formatTest/unit_tests/input/basicStructures.re
@@ -593,6 +593,19 @@ module Foo = {
   type bar = {foo: Baz.foo};
 };
 
+/* Don't pun field names that aren't "simple" */
+type foo = {
+  bar: Baz.bar,
+  qux: qux,
+  fooo: Fooo.fooo
+};
+
+let moreFoo = {
+  bar: Baz.bar,
+  qux: qux,
+  fooo: Fooo.fooo
+};
+
 /* record value punning */
 
 let props = {title: "hi"};