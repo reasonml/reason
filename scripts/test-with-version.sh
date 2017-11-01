ae5c7673a22ef0e30284c34e5596a40299a49995
Jordan Walke <jordojw@gmail.com>
2017-11-01 00:37:49 -0700

    Our constraints might be wrong - certain menhir versions fail

--- scripts/test-with-version.sh
+++ scripts/test-with-version.sh
@@ -7,7 +7,10 @@ export OCAML_VERSION="${1}"
 make clean
 opam switch "${OCAML_VERSION}"
 eval `opam config env`
+opam update
 opam install -y jbuilder
+# Our constraints are wrong I believe. We need this version.
+opam install -y menhir.20170712
 opam pin add -y reason .
 make test
 git diff --exit-code
