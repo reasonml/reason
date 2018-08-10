reasonml/reason

commit 7ade7ef699374b9abc315fc88ce7aa65ebb24dc2

Fix inconsistent printing of opening extension expressions. (#1979)
The `open` expr `open Syntax` contains an extension as child expression.
This is a "sequency" expression and should not be formatted with the
open syntax sugar.

The following should be formatted as is:

let work = () => {
  open Syntax;
  let%bind name = x;
  name;
};

src/reason-parser/reason_pprint_ast.ml
@@ -5385,6 +5385,7 @@ let printer = object(self:'self)
         | ([], Pexp_open (ovf, lid, e)) ->
           ovf == Fresh && self#isSeriesOfOpensFollowedByNonSequencyExpression e
         | ([], Pexp_letexception _) -> false
+        | ([], Pexp_extension _) -> false
         | _ -> true
 
   method unparseObject ?wrap:((lwrap,rwrap)=("", "")) ?(withStringKeys=false) l o =

formatTest/unit_tests/expected_output/extensions.re
@@ -349,3 +349,9 @@ let () = {
 };
 
 [%bs.raw x => x];
+
+let work = () => {
+  open Syntax;
+  let%bind name = x;
+  name;
+};

formatTest/unit_tests/input/extensions.re
@@ -323,3 +323,5 @@ let () = {
 };
 
 [%bs.raw x => x];
+
+let work = () => { open Syntax; let%bind name = x; name; }

commit 01fde3d0bad67ab96c51566d942486742209df9c

Fix local open printing for `bs.obj`
This fixes a regression introduced in https://github.com/facebook/reason/pull/1979

src/reason-parser/reason_pprint_ast.ml
@@ -5751,7 +5751,7 @@ let printer = object(self:'self)
         | ([], Pexp_open (ovf, lid, e)) ->
           ovf == Fresh && self#isSeriesOfOpensFollowedByNonSequencyExpression e
         | ([], Pexp_letexception _) -> false
-        | ([], Pexp_extension _) -> false
+        | ([], Pexp_extension ({txt}, _)) -> txt = "bs.obj"
         | _ -> true
 
   method unparseObject ?wrap:((lwrap,rwrap)=("", "")) ?(withStringKeys=false) l o =

formatTest/unit_tests/expected_output/bucklescript.re
@@ -119,3 +119,6 @@ type y = {
   "height":
     (int, int, int, float, float, float) => unit,
 };
+
+/* https://github.com/facebook/reason/issues/2121 */
+Style.({"container": 3});

formatTest/unit_tests/input/bucklescript.re
@@ -90,3 +90,6 @@ type y = {
   [@foo barbaz] "width": (int, int, int, float, float, float) => unit,
   [@foo barbaz] "height": (int, int, int, float, float, float) => unit,
 };
+
+/* https://github.com/facebook/reason/issues/2121 */
+Style.( { "container": 3 });