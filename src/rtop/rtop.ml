9934a390fa4ae1840b513a9613d53d25e1f63309
Ulrik Strid <ulrik.strid@outlook.com>
2018-09-11 17:23:08 +0200

    Format with ocp-indent

--- src/rtop/rtop.ml
+++ src/rtop/rtop.ml
@@ -6,31 +6,31 @@ in
 (* Try to read the HOME environment
  * if it is not set we assume it's windows and fallback to USERPROFILE *)
 let home = match getenv_opt "HOME" with
-| Some(p) -> p
-| None -> Sys.getenv "USERPROFILE"
+  | Some(p) -> p
+  | None -> Sys.getenv "USERPROFILE"
 in
 
 let () = Unix.openfile (Filename.concat home ".utoprc") [] 0o640
-|> Unix.close in
+         |> Unix.close in
 
 let () = Unix.openfile (Filename.concat home ".utop-history") [] 0o640
-|> Unix.close in
+         |> Unix.close in
 
 (* Get the dir of the current executable *)
 let dir = Filename.dirname Sys.executable_name in
 
 (* If there is only 1 arg it's the exeuctable and we can remove that *)
 let argv = match Array.length Sys.argv with
-| 1 -> [||]
-| argLength -> Array.sub Sys.argv 1 argLength
+  | 1 -> [||]
+  | argLength -> Array.sub Sys.argv 1 argLength
 in
 
 let args = if Array.length argv == 1 && argv.(0) = "stdin" then
-  let refmted = Pervasives.input_line (Unix.open_process_in "refmt --parse re --print ml --interface false") in
+    let refmted = Pervasives.input_line (Unix.open_process_in "refmt --parse re --print ml --interface false") in
 
-  Array.concat [[|"utop-full";|]; argv; [|refmted|]]
-else
-  Array.concat [[|"utop-full"; "-init"; Filename.concat dir "rtop_init.ml";|]; argv; [|"-I"; home; "-safe-string"|]]
+    Array.concat [[|"utop-full";|]; argv; [|refmted|]]
+  else
+    Array.concat [[|"utop-full"; "-init"; Filename.concat dir "rtop_init.ml";|]; argv; [|"-I"; home; "-safe-string"|]]
 in
 
 Unix.execvp "utop-full" args
