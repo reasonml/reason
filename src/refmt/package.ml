6d2b011bc554e79a65bf33b11835765b130d6ec8
Jordan Walke <jordojw@gmail.com>
2018-10-21 02:17:31 -0700

    Properly Bump version and make sure shows up in refmt --version

--- src/refmt/package.ml
+++ src/refmt/package.ml
@@ -1,4 +1,4 @@
 
-let version = "3.3.3"
-let git_version = "fefe5e4db3a54a7946c2220ee037dd2f407011c9"
-let git_short_version = "fefe5e4d"
+let version = "3.3.6"
+let git_version = "71d70bcf0d9799e3ec199c3a202cd20d9e979c06"
+let git_short_version = "71d70bc"
