d76fc763ff904527354c426b18d2ac4d79dd1ca8
Cheng Lou <chenglou92@gmail.com>
2019-03-23 03:00:03 -0700

    Sync back package.ml for 3.4.1 (#2358)

--- src/refmt/package.ml
+++ src/refmt/package.ml
@@ -1,4 +1,4 @@
 
-let version = "3.4.0"
-let git_version = "681c491ba760cdf3b49f92297c3dab1703682808"
-let git_short_version = "681c491"
+let version = "3.4.1"
+let git_version = "ea3fc2c1ebc63ba81e718da8740e6b20f24dafd6"
+let git_short_version = "ea3fc2c"
