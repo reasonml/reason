54958be75e15be88158a1ffe0c94ca912483ea1b
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2019-04-14 19:18:05 +0100

    forgot to commit the tests

---
+++ formatTest/unit_tests/expected_output/ocaml_identifiers.re
@@ -0,0 +1,67 @@
+/* Type names (supported with PR#2342) */
+module T = {
+  type pub_ = unit;
+};
+
+/* Value names (already supported) */
+module V = {
+  let pub_ = ();
+};
+
+/* Record fields */
+module R = {
+  type r = {pub_: int};
+
+  let foo = {pub_: 4};
+};
+
+/* Class names and instance variables */
+module C = {
+  class pub_ = {
+    as _;
+  };
+
+  class c = {
+    as _;
+    val pub_ = 0;
+    pub pub_ = () => ();
+  };
+
+  class c' = {
+    as _;
+    inherit class pub_;
+    val! pub_ = 1;
+  };
+};
+
+/* Class types */
+module Ct = {
+  class type pub_ = {
+    val pub_: unit => unit;
+  };
+};
+
+/* Virtual */
+module Cv = {
+  class virtual pub_ = {
+    as _;
+  };
+};
+
+/* Object methods */
+module O = {
+  let o = {as _; pub pub_ = ()};
+};
+
+/* Function parameter labels */
+module L = {
+  let f = (~pub_) => ignore(pub_);
+};
+
+/* Module types */
+module type pub_ = {};
+
+/* Polymorphic variants (probably ok as-is?) */
+module P = {
+  type t = [ | `pub_];
+};
---
+++ formatTest/unit_tests/input/ocaml_identifiers.ml
@@ -0,0 +1,62 @@
+(* Type names (supported with PR#2342) *)
+module T = struct
+  type pub = unit
+end
+
+(* Value names (already supported) *)
+module V = struct
+  let pub = ()
+end
+
+(* Record fields *)
+module R = struct
+  type r = { pub : int }
+
+  let foo = { pub = 4 }
+end
+
+(* Class names and instance variables *)
+module C = struct
+  class pub = object end
+
+  class c = object
+    val pub = 0
+    method pub () = ()
+  end
+
+  class c' = object
+    inherit pub
+    val! pub = 1
+  end
+end
+
+(* Class types *)
+module Ct = struct
+  class type pub = object
+    val pub: unit -> unit
+  end
+end
+
+(* Virtual *)
+module Cv = struct
+  class virtual pub = object end
+end
+
+(* Object methods *)
+module O = struct
+  let o = object method pub = () end
+end
+
+(* Function parameter labels *)
+module L = struct
+  let f ~pub = ignore pub
+end
+
+(* Module types *)
+module type pub = sig
+end
+
+(* Polymorphic variants (probably ok as-is?) *)
+module P = struct
+  type t = [ `pub ]
+end
