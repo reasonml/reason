eb5b2bde5518c216ad97274f62b60c411e6ca27c
Frédéric Bour <frederic.bour@lakaban.net>
2019-04-29 22:47:03 +0200

    add recovery tool

---
+++ src/menhir-recover/emitter.mli
@@ -0,0 +1,13 @@
+open MenhirSdk.Cmly_api
+open Attributes
+open Synthesis
+open Recovery_intf
+
+module Make
+    (G : GRAMMAR)
+    (A : ATTRIBUTES with module G := G)
+    (S : SYNTHESIZER with module G := G)
+    (R : RECOVERY with module G := G) :
+sig
+  val emit : Format.formatter -> unit
+end
