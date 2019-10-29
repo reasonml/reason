441934492293980bf5b6fc82d320ceb444a42358
Jordan W <jordojw@gmail.com>
2019-10-29 03:39:51 -0700

    Fix releases for multi-bin release configs (#2482)
    * Fix releases for multi-bin release configs
    
    * one more fix for JS
    
    * Speed up CI cache on windows and log problems
    
    * a fix
    
    * Speed up CI cache on windows and log problems

--- .ci/pipelines-release.js
+++ .ci/pipelines-release.js
@@ -25,8 +25,7 @@ const bins =
     (acc, currKey) => Object.assign({ [currKey]: "bin/" + mainPackageJson.esy.release.bin[currKey] }, acc),
     {}
   );
-  
-console.log(bins);
+
 const packageJson = JSON.stringify(
   {
     name: mainPackageJson.name,
@@ -87,13 +86,21 @@ const placeholderFile = `:; echo "You need to have postinstall enabled"; exit $?
 @ECHO OFF
 ECHO You need to have postinstall enabled`;
 fs.mkdirSync(path.join(__dirname, "..", "_release", "bin"));
-const binPath = path.join(
-  __dirname,
-  "..",
-  "_release",
-  "bin",
-  mainPackageJson.esy.release.bin[0]
-);
 
-fs.writeFileSync(binPath, placeholderFile);
-fs.chmodSync(binPath, 0777);
+Object.keys(bins).forEach(
+  name => {
+    if(bins[name]) {
+      const binPath = path.join(
+        __dirname,
+        "..",
+        "_release",
+        bins[name]
+      );
+      fs.writeFileSync(binPath, placeholderFile);
+      fs.chmodSync(binPath, 0777);
+    } else {
+      console.log("bins[name] name=" + name + " was empty. Weird.");
+      console.log(bins);
+    }
+  }
+);
