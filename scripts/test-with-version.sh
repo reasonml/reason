df331819cabc2748f57b0e3d8d301f152ed99ad0
David Sancho <dsnxmoreno@gmail.com>
2021-04-05 23:49:17 +0200

    Run opam exec

--- scripts/test-with-version.sh
+++ scripts/test-with-version.sh
@@ -8,10 +8,7 @@ make clean-for-ci
 opam switch "${OCAML_VERSION}"
 eval `opam config env`
 opam update
-opam install -y dune
 # Our constraints are wrong I believe. We need this version.
-opam install -y menhir
-opam pin add -y reason .
-opam pin add -y rtop .
+make install
 make test-ci
 git diff --exit-code
