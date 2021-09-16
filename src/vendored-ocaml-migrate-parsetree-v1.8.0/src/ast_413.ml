bd3256c7318c099c605024c85dc251f882683d10
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-09-16 16:58:53 -0700

    same fix for 4.13

--- src/vendored-ocaml-migrate-parsetree-v1.8.0/src/ast_413.ml
+++ src/vendored-ocaml-migrate-parsetree-v1.8.0/src/ast_413.ml
@@ -1265,20 +1265,20 @@ let docstrings : docstring list ref = ref []
 (* Warn for unused and ambiguous docstrings *)
 
 let warn_bad_docstrings () =
-  if Warnings.is_active (Warnings.Unexpected_docstring true) then begin
+  if Warnings.is_active (Migrate_parsetree_compiler_functions.bad_docstring true) then begin
     List.iter
       (fun ds ->
          match ds.ds_attached with
          | Info -> ()
          | Unattached ->
-           prerr_warning ds.ds_loc (Warnings.Unexpected_docstring true)
+             prerr_warning ds.ds_loc (Migrate_parsetree_compiler_functions.bad_docstring true)
          | Docs ->
              match ds.ds_associated with
              | Zero | One -> ()
              | Many ->
-               prerr_warning ds.ds_loc (Warnings.Unexpected_docstring false))
+                 prerr_warning ds.ds_loc (Migrate_parsetree_compiler_functions.bad_docstring false))
       (List.rev !docstrings)
-end
+  end
 
 (* Docstring constructors and destructors *)
 
