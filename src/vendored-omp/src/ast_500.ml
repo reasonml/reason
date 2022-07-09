3f909247b0a33d83f5f81b4b530a038f1b40e6e9
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-07-08 17:25:05 -0700

    fix magic numbers for 5.0 (#2671)

--- src/vendored-omp/src/ast_500.ml
+++ src/vendored-omp/src/ast_500.ml
@@ -4164,8 +4164,8 @@ module Outcometree = struct
     | Ophr_exception of (exn * out_value)
 end
 module Config = struct
-  let ast_impl_magic_number = "Caml1999M031"
-  let ast_intf_magic_number = "Caml1999N031"
+  let ast_impl_magic_number = "Caml1999M032"
+  let ast_intf_magic_number = "Caml1999N032"
 end
 
 
