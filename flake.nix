8d607680b1437d967b91ac979d1d8ae8d051cc64
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-10-15 12:13:53 -0700

    Prepare for 3.8.2

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
   description = "Nix Flake for ReasonML";
 
+  inputs.nix-filter.url = "github:numtide/nix-filter";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:anmonteiro/nix-overlays";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nix-filter }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages."${system}".extend (self: super: {
@@ -12,7 +13,17 @@
         });
       in
       rec {
-        packages.default = pkgs.callPackage ./nix { };
-        devShells.default = pkgs.callPackage ./shell.nix { };
+        packages.default = pkgs.callPackage ./nix {
+          nix-filter = nix-filter.lib;
+        };
+        devShells = {
+          default = pkgs.callPackage ./nix/shell.nix {
+            reason = packages.default;
+          };
+          release = pkgs.callPackage ./nix/shell.nix {
+            reason = packages.default;
+            release-mode = true;
+          };
+        };
       });
 }
