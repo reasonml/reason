ae606403151b2c8a16ae84b772fa7b7eb229c41d
David Sancho Moreno <dsnxmoreno@gmail.com>
2023-03-07 09:30:40 +0100

    refactor(refmt) Make run-ocamlc available to run everywhere

--- refmt_test/run-ocamlc.sh
+++ refmt_test/run-ocamlc.sh
@@ -4,9 +4,11 @@ set -eu
 
 unameOut="$(uname -s)"
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 case "${unameOut}" in
     MINGW*)     REFMT_PATH=$(cygpath --mixed --absolute $(which refmt.exe));;
-    *)          REFMT_PATH="../src/refmt/refmt_impl.exe"
+    *)          REFMT_PATH="$SCRIPT_DIR/../src/refmt/refmt_impl.exe"
 esac
 
 ocamlc -c -pp "$REFMT_PATH --print binary" "$@"
