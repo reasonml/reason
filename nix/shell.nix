c998144eeeedb6eb8035d11f7af2e522a77892a9
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-08-11 16:54:43 -0700

    chore: format the codebase with ocamlformat (#2789)

--- nix/shell.nix
+++ nix/shell.nix
@@ -11,7 +11,7 @@
 
 mkShell {
   inputsFrom = [ reason ];
+  nativeBuildInputs = with ocamlPackages; [ utop merlin odoc ocamlformat ];
   buildInputs =
-    with ocamlPackages; [ utop merlin odoc ]
-      ++ (if release-mode then [ cacert curl dune-release git ] else [ ]);
+    with ocamlPackages; (if release-mode then [ cacert curl dune-release git ] else [ ]);
 }
