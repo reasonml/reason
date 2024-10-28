2e1d6d194980d5e28ff5377802da8ff8e7b5eee5
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-10-27 18:48:51 -0700

    add merlin-extend lower bounds

--- nix/shell.nix
+++ nix/shell.nix
@@ -7,11 +7,17 @@
 , curl
 , git
 , release-mode ? false
+, pkgs
 }:
 
 mkShell {
   inputsFrom = [ reason ];
-  nativeBuildInputs = with ocamlPackages; [ utop merlin odoc ocamlformat ];
+  nativeBuildInputs = with ocamlPackages; [
+    utop
+    merlin
+    # odoc
+    pkgs.ocaml-ng.ocamlPackages_5_2.ocamlformat
+  ];
   buildInputs =
     with ocamlPackages; (if release-mode then [ cacert curl dune-release git ] else [ ]);
 }
