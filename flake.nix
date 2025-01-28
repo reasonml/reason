1d0e6d47587398368ab86bca801932be8ddee5a4
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-01-27 22:48:57 -0800

    start migrating to ppxlib 5.2 ast

--- flake.nix
+++ flake.nix
@@ -8,7 +8,17 @@
       forAllSystems = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
         let
           pkgs = nixpkgs.legacyPackages.${system}.extend (self: super: {
-            ocamlPackages = super.ocaml-ng.ocamlPackages_5_3;
+            ocamlPackages = super.ocaml-ng.ocamlPackages_5_3.overrideScope (oself: osuper: {
+              ppxlib = osuper.ppxlib.overrideAttrs (_: {
+                src = super.fetchFromGitHub {
+                  owner = "ocaml-ppx";
+                  repo = "ppxlib";
+                  rev = "30ebdf9c31b14b8efb23b11575e97c6a43aa4554";
+                  hash = "sha256-NPerzuytyeKhssiNOA3uufy3thbWnRCkDLXR2yfR//k=";
+                };
+              });
+
+            });
           });
         in
         f pkgs);
