32ad4be39d28bb0edb6726fea3a8a90a914a2349
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-02-09 00:08:49 -0800

    no option

--- src/refmt/eol_detect.ml
+++ src/refmt/eol_detect.ml
@@ -9,9 +9,9 @@ let get_eol_for_file filename =
   let ic = open_in_bin filename in
   let rec loop prev =
     match input_char ic with
-    | '\n' -> (match prev with Some '\r' -> CRLF | _ -> LF)
-    | c -> loop (Some c)
+    | '\n' -> if prev = '\r' then CRLF else LF
+    | c -> loop c
   in
-  let eol = try loop None with End_of_file -> default_eol in
+  let eol = try loop ' ' with End_of_file -> default_eol in
   close_in ic;
   eol
