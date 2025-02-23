72fd965fdfdaf8d31db60d6d5283e174bccd79d7
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-02-22 22:44:18 -0800

    try to get tests running

--- nix/ci.nix
+++ nix/ci.nix
@@ -22,6 +22,9 @@ let
                 hash = "sha256-KfuA31hmFHpPkp7lq7lH6jaQyLiqBd/UnY5+ctntmF0=";
               };
             });
+            pp = osuper.pp.overrideAttrs (_: {
+              doCheck = false;
+            });
           });
         })
       ];
