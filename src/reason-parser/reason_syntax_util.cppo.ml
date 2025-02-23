dbea8a50f1760b251c4b152cfc046d6a0da4fbe0
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-02-22 23:05:04 -0800

    fix: Pparam_newtype / ocaml_identifiers

--- src/reason-parser/reason_syntax_util.cppo.ml
+++ src/reason-parser/reason_syntax_util.cppo.ml
@@ -420,13 +420,16 @@ class identifier_mapper f =
         match expr with
         | { pexp_desc = Pexp_ident lid;_ } ->
           { expr with pexp_desc = Pexp_ident (map_lid lid) }
-        | { pexp_desc = Pexp_function (params, constraint_, body); _ } when !rename_labels ->
-          let new_params = List.map (fun param ->
-            match param with
-            | { pparam_desc = Pparam_val (lbl, eo, pat); _ } ->
-              { param with pparam_desc = Pparam_val (map_label lbl, eo, pat) }
-            | { pparam_desc = Pparam_newtype s; _ } ->
-              { param with pparam_desc = Pparam_newtype { s with txt = f s.txt } }) params
+        | { pexp_desc = Pexp_function (params, constraint_, body); _ } ->
+          let new_params =
+            List.map (fun param ->
+              match param with
+              | { pparam_desc = Pparam_val (lbl, eo, pat); _ } when !rename_labels ->
+                { param with pparam_desc = Pparam_val (map_label lbl, eo, pat) }
+              | { pparam_desc = Pparam_newtype s; _ } ->
+                { param with pparam_desc = Pparam_newtype { s with txt = f s.txt } }
+              | _ -> param)
+            params
           in
           { expr with pexp_desc = Pexp_function (new_params, constraint_, body) }
         | { pexp_desc = Pexp_apply (e, args);_ } when !rename_labels ->
