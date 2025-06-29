345d58d125c81e63e9ad7104001fd66c96e2ad7d
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-06-29 14:27:23 -0700

    refmt: refactor end of line detection support

--- test/lib/fdLeak.ml
+++ test/lib/fdLeak.ml
@@ -4,6 +4,6 @@ let () =
   let files = List.init 25 (fun i -> i) in
   let input_file = "./input.re" in
   List.iter
-    (fun _file -> Eol_detect.get_eol_for_file input_file |> ignore)
+    (fun _file -> End_of_line.Detect.get_eol_for_file input_file |> ignore)
     files;
   Format.eprintf "EOL: done@."
