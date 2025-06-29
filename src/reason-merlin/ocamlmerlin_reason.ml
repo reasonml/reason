68173a4b3f096bf2152c6bd57d239cdb1d472419
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-06-29 13:23:45 -0700

    ocamlmerlin-reason: tighten bindings, remove `open`s

--- src/reason-merlin/ocamlmerlin_reason.ml
+++ src/reason-merlin/ocamlmerlin_reason.ml
@@ -1,9 +1,10 @@
 open Reason
-open Extend_protocol.Reader
 
 let () = Reason_config.recoverable := true
 
 module Reason_reader = struct
+  open Extend_protocol.Reader
+
   type t = buffer
 
   let load buffer = buffer
@@ -19,18 +20,18 @@ module Reason_reader = struct
 
   let signature sg =
     let sg =
+      let open Reason_syntax_util in
       sg
-      |> Reason_syntax_util.(
-           apply_mapper_to_signature remove_stylistic_attrs_mapper)
-      |> Reason_syntax_util.(apply_mapper_to_signature backport_letopt_mapper)
+      |> apply_mapper_to_signature remove_stylistic_attrs_mapper
+      |> apply_mapper_to_signature backport_letopt_mapper
     in
     Signature (Reason_toolchain.To_current.copy_signature sg)
 
   let parse { text; path; _ } =
-    let l = String.length path in
     let buf = Lexing.from_string text in
     Location.init buf (Filename.basename path);
-    if l > 0 && path.[l - 1] = 'i'
+    let l = String.length path in
+    if l > 0 && String.unsafe_get path (l - 1) = 'i'
     then signature (Reason_toolchain.RE.interface buf)
     else structure (Reason_toolchain.RE.implementation buf)
 
@@ -51,52 +52,61 @@ module Reason_reader = struct
     let fmt = lazy (Reason_pprint_ast.createFormatter ()) in
     fun () -> Lazy.force fmt
 
-  let pretty_print ppf =
-    let open Reason_toolchain in
-    function
-    | Pretty_core_type x ->
-      (formatter ())#core_type ppf (From_current.copy_core_type x)
-    | Pretty_case_list x ->
-      (formatter ())#case_list ppf (List.map From_current.copy_case x)
-    | Pretty_expression x ->
-      (formatter ())#expression ppf (From_current.copy_expression x)
-    | Pretty_pattern x ->
-      (formatter ())#pattern ppf (From_current.copy_pattern x)
-    | Pretty_signature x ->
-      (formatter ())#signature [] ppf (From_current.copy_signature x)
-    | Pretty_structure x ->
-      (formatter ())#structure [] ppf (From_current.copy_structure x)
-    | Pretty_toplevel_phrase x ->
-      (formatter ())#toplevel_phrase ppf (From_current.copy_toplevel_phrase x)
+  let pretty_print =
+    let module From_current = Reason_toolchain.From_current in
+    fun ppf ppt ->
+      let fmt = formatter () in
+      match ppt with
+      | Pretty_core_type x -> fmt#core_type ppf (From_current.copy_core_type x)
+      | Pretty_case_list x ->
+        fmt#case_list ppf (List.map From_current.copy_case x)
+      | Pretty_expression x ->
+        fmt#expression ppf (From_current.copy_expression x)
+      | Pretty_pattern x -> fmt#pattern ppf (From_current.copy_pattern x)
+      | Pretty_signature x ->
+        fmt#signature [] ppf (From_current.copy_signature x)
+      | Pretty_structure x ->
+        fmt#structure [] ppf (From_current.copy_structure x)
+      | Pretty_toplevel_phrase x ->
+        fmt#toplevel_phrase ppf (From_current.copy_toplevel_phrase x)
 
-  let print_outcome ppf =
-    let open Reason_toolchain in
-    let open Reason_oprint in
-    function
-    | Out_value x -> print_out_value ppf (From_current.copy_out_value x)
-    | Out_type x -> print_out_type ppf (From_current.copy_out_type x)
-    | Out_class_type x ->
-      print_out_class_type ppf (From_current.copy_out_class_type x)
-    | Out_module_type x ->
-      print_out_module_type ppf (From_current.copy_out_module_type x)
-    | Out_sig_item x ->
-      print_out_sig_item ppf (From_current.copy_out_sig_item x)
-    | Out_signature x ->
-      print_out_signature ppf (List.map From_current.copy_out_sig_item x)
-    | Out_type_extension x ->
-      print_out_type_extension ppf (From_current.copy_out_type_extension x)
-    | Out_phrase x -> print_out_phrase ppf (From_current.copy_out_phrase x)
+  let print_outcome =
+    let module From_current = Reason_toolchain.From_current in
+    fun ppf otree ->
+      match otree with
+      | Out_value x ->
+        Reason_oprint.print_out_value ppf (From_current.copy_out_value x)
+      | Out_type x ->
+        Reason_oprint.print_out_type ppf (From_current.copy_out_type x)
+      | Out_class_type x ->
+        Reason_oprint.print_out_class_type
+          ppf
+          (From_current.copy_out_class_type x)
+      | Out_module_type x ->
+        Reason_oprint.print_out_module_type
+          ppf
+          (From_current.copy_out_module_type x)
+      | Out_sig_item x ->
+        Reason_oprint.print_out_sig_item ppf (From_current.copy_out_sig_item x)
+      | Out_signature x ->
+        Reason_oprint.print_out_signature
+          ppf
+          (List.map From_current.copy_out_sig_item x)
+      | Out_type_extension x ->
+        Reason_oprint.print_out_type_extension
+          ppf
+          (From_current.copy_out_type_extension x)
+      | Out_phrase x ->
+        Reason_oprint.print_out_phrase ppf (From_current.copy_out_phrase x)
 end
 
 let () =
-  let open Extend_main in
-  let _ =
-    match Sys.win32 with
-    | true ->
-      set_binary_mode_in stdin true;
-      set_binary_mode_out stdout true
-    | _ -> ()
-  in
-  extension_main
-    ~reader:(Reader.make_v0 (module Reason_reader : V0))
-    (Description.make_v0 ~name:"reason" ~version:"0.1")
+  if Sys.win32
+  then (
+    set_binary_mode_in stdin true;
+    set_binary_mode_out stdout true);
+  Extend_main.extension_main
+    ~reader:
+      (Extend_main.Reader.make_v0
+         (module Reason_reader : Extend_protocol.Reader.V0))
+    (Extend_main.Description.make_v0 ~name:"reason" ~version:"0.1")
