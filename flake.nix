7e0d5a35e350b8793e5587ab4edd605528982ecb
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-01-19 20:33:22 -0800

    build: format nix files with nixfmt, update nix flakes

--- flake.nix
+++ flake.nix
@@ -3,20 +3,33 @@
 
   inputs.nixpkgs.url = "github:nix-ocaml/nix-overlays";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      forAllSystems = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system}.extend (self: super: {
-            ocamlPackages = super.ocaml-ng.ocamlPackages_5_4;
-          });
-        in
-        f pkgs);
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system}.extend (
+              self: super: {
+                ocamlPackages = super.ocaml-ng.ocamlPackages_5_4;
+              }
+            );
+          in
+          f pkgs
+        );
     in
     {
-      packages = forAllSystems (pkgs:
-        let packages = pkgs.callPackage ./nix { }; in
-        { inherit packages; default = packages.reason; }
+      packages = forAllSystems (
+        pkgs:
+        let
+          packages = pkgs.callPackage ./nix { };
+        in
+        {
+          inherit packages;
+          default = packages.reason;
+        }
       );
 
       devShells = forAllSystems (pkgs: {
